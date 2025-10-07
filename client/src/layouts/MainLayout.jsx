fc3fb4a20b4a98eb6baafc578ad966abba3134fa
dandimuzaki <dandimuzaki@gmail.com>
2025-10-07 23:21:22 +0700

    Enhancement: Responsive design

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <>
       <Sidebar />
-      <div className='md:ml-50 md:px-12 px-4 bg-[var(--background)] pb-20'>
+      <div className='md:ml-50 md:px-12 px-4 bg-[var(--background)] w-screen md:w-fit pb-20'>
       <Outlet />
       </div>
     </>
